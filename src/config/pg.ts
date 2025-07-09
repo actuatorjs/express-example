81d545338d677fdedc991dcc20598afd922c6220
hadestructhor <60148800+hadestructhor@users.noreply.github.com>
2025-07-10 00:57:28 +0200

    fix(env): added database env for host

--- src/config/pg.ts
+++ src/config/pg.ts
@@ -1,9 +1,10 @@
 import { Pool } from "pg";
+import * as process from "node:process";
 
 export const pool: Pool = new Pool({
-	host: "localhost",
-	port: 5432,
-	user: "user",
-	password: "pass",
-	database: "db",
+  host: process.env.DB_HOST,
+  port: 5432,
+  user: "user",
+  password: "pass",
+  database: "db",
 });
